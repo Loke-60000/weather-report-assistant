2e4a1e2671fc04024eef62f6937dc511b9edd9d2
Lokman <ramdani.lokman@gmail.com>
2024-03-22 14:29:52 +0100

    tests added

---
+++ data_collector_weather/main.py
@@ -0,0 +1,31 @@
+from meteofrance_api import MeteoFranceClient
+from cities import cities
+from functions.db_and_table_init import connect_to_database, create_table
+from functions.insert_data import insert_weather_data
+from functions.delete_data import delete_former_data
+
+
+def run_daily_batch():
+    client = MeteoFranceClient()
+    conn = connect_to_database()
+    create_table(conn)
+
+    for c in cities:
+        list_places = client.search_places(c)
+        if list_places:
+            my_place = list_places[0]
+
+            my_place_weather_forecast = client.get_forecast_for_place(my_place)
+            if my_place_weather_forecast and hasattr(my_place_weather_forecast, 'forecast'):
+                my_place_hourly_forecast = my_place_weather_forecast.forecast
+
+                for hour in my_place_hourly_forecast:
+                    insert_weather_data(
+                        conn, client, my_place, my_place_weather_forecast, hour)
+
+    delete_former_data(conn)
+    conn.commit()
+    conn.close()
+
+
+run_daily_batch()
---
+++ data_collector_weather/test_daily_main.py
@@ -0,0 +1,88 @@
+import pytest
+from unittest.mock import MagicMock
+from functions.db_and_table_init import connect_to_database
+from meteofrance_api.helpers import readeable_phenomenoms_dict
+from meteofrance_api import MeteoFranceClient
+from meteofrance_api import MeteoFranceClient
+
+DATABASE = "franceForecast"
+USER = "lokman"
+PASSWORD = "lokman"
+HOST = "localhost"
+PORT = "5432"
+
+city_list = ["Paris", "Montpellier", "Lyon", "Marseille", "Toulouse",]
+
+
+@pytest.mark.fixture("mock_dependencies")
+def test_connect_to_database():
+    conn = connect_to_database()
+    assert conn is not None, "Connection to the database failed."
+
+
+@pytest.mark.parametrize("city", city_list)
+def test_search_places(city: str) -> None:
+    client = MeteoFranceClient()
+    list_places = client.search_places(city)
+    assert list_places
+
+
+@pytest.mark.parametrize("city", city_list)
+def test_get_forecast_for_place(city: str) -> None:
+    client = MeteoFranceClient()
+    list_places = client.search_places(city)
+    my_place = list_places[0]
+    my_place_weather_forecast = client.get_forecast_for_place(my_place)
+    assert my_place_weather_forecast
+
+
+@pytest.mark.parametrize("city", city_list)
+def test_get_daily_forecast_for_place(city: str) -> None:
+    client = MeteoFranceClient()
+    list_places = client.search_places(city)
+    my_place = list_places[0]
+    my_place_weather_forecast = client.get_forecast_for_place(my_place)
+    my_place_daily_forecast = my_place_weather_forecast.daily_forecast
+    assert isinstance(my_place_daily_forecast, list)
+
+
+@pytest.mark.parametrize("city", city_list)
+def test_get_rain_forecast(city: str) -> None:
+    """
+    Test if the rain forecast is available for the given city.
+    """
+    client = MeteoFranceClient()
+    list_places = client.search_places(city)
+    my_place = list_places[0]
+    my_place_weather_forecast = client.get_forecast_for_place(my_place)
+    if my_place_weather_forecast.position["rain_product_available"] == 1:
+        my_place_rain_forecast = client.get_rain(
+            my_place.latitude, my_place.longitude)
+        next_rain_dt = my_place_rain_forecast.next_rain_date_locale()
+        rain_status = next_rain_dt.strftime(
+            "%H:%M") if next_rain_dt else "No rain expected in the following hour."
+    else:
+        rain_status = "No rain forecast available."
+    assert rain_status
+
+
+@pytest.mark.parametrize("city", city_list)
+def test_get_weather_warnings(city: str) -> None:
+    """
+    Test if the weather warnings are available for the given city.
+    """
+    client = MeteoFranceClient()
+    list_places = client.search_places(city)
+    my_place = list_places[0]
+    readable_warnings = {}
+    if my_place.admin2:
+        my_place_weather_alerts = client.get_warning_current_phenomenoms(
+            my_place.admin2)
+        readable_warnings = readeable_phenomenoms_dict(
+            my_place_weather_alerts.phenomenons_max_colors)
+    assert isinstance(readable_warnings, dict)
+
+
+def test_connect_to_database():
+    conn = connect_to_database()
+    assert conn is not None, "Connection to the database failed."
